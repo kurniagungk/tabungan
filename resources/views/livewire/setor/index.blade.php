dfae1103978a5ff154004a6aef6919318581d6b2
kurniagungk <37070156+kurniagungk@users.noreply.github.com>
2020-08-12 07:18:49 +0700

    fix setor

--- resources/views/livewire/setor/index.blade.php
+++ resources/views/livewire/setor/index.blade.php
@@ -1,7 +1,3 @@
-@extends('layouts.dashboard')
-
-@section('content')
-
 <div class="col-xl col-lg">
     <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
@@ -15,53 +11,73 @@
             </div>
         </div>
         <!-- search -->
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" autofocus placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
+
 
         <!-- Card Body -->
         <div class="card-body">
             <div class="row">
-                <div class="col-xl-4 col-lg-5">
+
+
+
+                <div class="col-xl-12 col-lg-12">
+
+                    @if (session()->has('pesan'))
+                    <div class="alert alert-success">
+                        {{ session('pesan') }}
+                    </div>
+                    @endif
+
                     <div class="card shadow mb-4">
                         <div class="card-body">
+                            <div class="input-group">
+                                <input wire:model.lazy="search" type="text" class="form-control " autofocus placeholder="Search for..." autocomplete="new-password">
+                                <div class="input-group-append">
+                                    <button wire:click='cari' class="btn btn-primary" type="button">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                @if($setor)
+                <div class="col-xl-4 col-lg-5">
+                    <div class="card shadow mb-4">
+
+                        <div class="card-body">
                             <div class="form-group row">
-                                <label for="staticEmail" class="col-sm-5 col-form-label">ID. Transaksi : </label>
+                                <label for="staticEmail" class="col-sm-4 col-form-label">Rekening : </label>
+                                <label for="staticEmail" class="col-sm-8 col-form-label">{{$santri->nis}}</label>
                             </div>
                             <div class="form-group row">
-                                <label for="staticEmail" class="col-sm-5 col-form-label">Rekening : </label>
+                                <label for="staticEmail" class="col-sm-4 col-form-label">Nama : </label>
+                                <label for="staticEmail" class="col-sm-8 col-form-label">{{$santri->nama}}</label>
                             </div>
                             <div class="form-group row">
-                                <label for="staticEmail" class="col-sm-7 col-form-label">Nama : </label>
+                                <label for="staticEmail" class="col-sm-4 col-form-label">Sekolah : </label>
+                                <label for="staticEmail" class="col-sm-8 col-form-label">{{$santri->sekolah}}</label>
                             </div>
                             <div class="form-group row">
-                                <label for="staticEmail" class="col-sm-7 col-form-label">Sekolah : </label>
-                            </div>
-                            <div class="form-group row">
-                                <label for="staticEmail" class="col-sm-7 col-form-label">Saldo : </label>
+                                <label for="staticEmail" class="col-sm-4 col-form-label">Saldo : </label>
+                                <label for="staticEmail" class="col-sm-8 col-form-label">{{$santri->saldo}}</label>
                             </div>
 
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Rp.</span>
                                 </div>
-                                <input type="number" class="form-control" autocomplete="off">
+                                <input wire:model.lazy="jumlah" type="number" class="form-control" autocomplete="off">
                             </div>
 
                             <center>
-                                <a class="btn btn-success btn-icon-split" href="#">
+                                <button wire:click="setor" class="btn btn-success btn-icon-split" href="#">
                                     <span class="icon text-white-50">
                                         <i class="fas fa-upload"></i>
                                     </span>
                                     <span class="text">Setor</span>
-                                </a>
+                                </button>
                             </center>
 
                         </div>
@@ -82,27 +98,23 @@
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    @foreach($transaksi as $t)
                                     <tr>
-                                        <td>2</td>
-                                        <td>2020-08-03 19:53</td>
-                                        <td>Rp. 0</td>
-                                        <td>Rp. 50.000 </td>
-                                        <td>Rp. 50.000</td>
+                                        <td>{{$loop->index}}</td>
+                                        <td>{{$t->created_at}}</td>
+                                        <td>{{$t->jumlah}}</td>
                                     </tr>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>2020-08-02 19:53</td>
-                                        <td>Rp. 100.000</td>
-                                        <td>Rp. 0 </td>
-                                        <td>Rp. 100.000</td>
-                                    </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
+                @endif
             </div>
+
         </div>
+
     </div>
 </div>
 
@@ -128,5 +140,3 @@
         </div>
     </div>
 </div>
-
-@endsection
