25b06167d01bb3485ace4e9fd0776bedcbdbe3d6
kurniagungk <37070156+kurniagungk@users.noreply.github.com>
2020-09-17 21:29:55 +0700

    fix

--- resources/views/livewire/transaksimitra/create.blade.php
+++ resources/views/livewire/transaksimitra/create.blade.php
@@ -132,8 +132,14 @@
                 @this.set('nis', content)
             });
             Instascan.Camera.getCameras().then(function(cameras) {
+
                 if (cameras.length > 0) {
-                    scanner.start(cameras[0]);
+                    if (cameras.length > 1) {
+                        scanner.start(cameras[1]);
+                    } else {
+                        scanner.start(cameras[0]);
+                    }
+
                 } else {
                     console.error('No cameras found.');
                 }
