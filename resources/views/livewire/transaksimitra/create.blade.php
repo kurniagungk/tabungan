325dde29dac1f4a0a1b46da19811de0c96ccde04
munir <badulmunir@gmail.com>
2020-09-19 21:49:24 +0700

    tampilan transaksi
    asd

--- resources/views/livewire/transaksimitra/create.blade.php
+++ resources/views/livewire/transaksimitra/create.blade.php
@@ -1,114 +1,116 @@
-<div class="row justify-content-center align-self-center ">
-    <div class="col-lg-6">
-        <div class="card o-hidden border-0 shadow-lg my-5 card-block">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
+<body class="bg-primary">
+    <div class="row justify-content-center align-self-center">
+        <div class="col-lg-10">
+            <div class="card o-hidden border-0 shadow-lg my-5 card-block">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
 
-
-                <div class="p-5">
-                    <div class="text-center">
-                        <h1 class="h5 text-gray-900 mb-4">Payment - {{$user}}</h1>
-                    </div>
-
-                    <div>
-                        @if (session()->has('message'))
-                        <div class="alert alert-success">
-                            {{ session('message') }}
-                        </div>
-                        @endif
-                    </div>
-
-                    <div>
-                        @if (session()->has('danger'))
-                        <div class="alert alert-danger">
-                            {{ session('danger') }}
-                        </div>
-                        @endif
-                    </div>
-
-
-                    <form wire:submit.prevent="bayar">
-
-
-                        <div class="input-group ">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Rp.</span>
-                            </div>
-                            <input wire:model.lazy="jumlah" id="jumlah" type="number" autofocus class="form-control form-control-user @error('jumlah') is-invalid @enderror">
-                            @error('jumlah')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div>
-                        <br>
-
-                        <!-- camera code -->
-
-
-
-                        <div class="input-group row @if($video) @else d-none @endif">
-                            <div class="embed-responsive embed-responsive-4by3">
-                                <video id="preview"></video>
-                            </div>
-
-                        </div>
-
-                        @if($video)
-                        <br>
-                        @endif
-
-                        <div class="input-group">
-
-                            <div class="input-group-prepend">
-                                <span wire:click="video" class="input-group-text"><i class="fas fa-user"></i></span>
-                            </div>
-                            <input value="" wire:model.debounce.500ms="nis" type="text" class="form-control form-control-user @error('nis') is-invalid @enderror" id="nis" placeholder="NIS Santri">
-                            @error('nis')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div>
-                        <br>
-
-                        @if($nasabah)
-
-                        <div class="form-group row">
-                            <label class="col-sm-12 col-form-label">Nama : {{$nasabah->nama}}</label>
-
-                        </div>
-
-                        @endif
-
-                        <div class="form-group row">
-                            <div class="col-sm-12 mb-12 mb-sm-0">
-                                <input wire:model="password" type="password" pattern="[0-9]*" inputmode="numeric" id="pin" class="form-control form-control-user @error('password') is-invalid @enderror" placeholder="PIN">
-                                @error('password')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h5 text-gray-900 mb-4">Payment - {{$user}}</h1>
                                 </div>
-                                @enderror
-                            </div>
 
+                                <div>
+                                    @if (session()->has('message'))
+                                    <div class="alert alert-success">
+                                        {{ session('message') }}
+                                    </div>
+                                    @endif
+                                </div>
+
+                                <div>
+                                    @if (session()->has('danger'))
+                                    <div class="alert alert-danger">
+                                        {{ session('danger') }}
+                                    </div>
+                                    @endif
+                                </div>
+
+                                <form wire:submit.prevent="bayar">
+
+                                    <div class="input-group ">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Rp.</span>
+                                        </div>
+                                        <input wire:model.lazy="jumlah" id="jumlah" type="number" autofocus class="form-control form-control-user @error('jumlah') is-invalid @enderror">
+                                        @error('jumlah')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                    </div>
+                                    <br>
+
+                                    <!-- camera code -->
+                                    <div class="input-group row @if($video) @else d-none @endif">
+                                        <div class="embed-responsive embed-responsive-4by3">
+                                            <video id="preview"></video>
+                                        </div>
+                                    </div>
+
+                                    @if($video)
+                                    <br>
+                                    @endif
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span wire:click="video" class="input-group-text"><i class="fas fa-user"></i></span>
+                                        </div>
+                                        <input value="" wire:model.debounce.500ms="nis" type="text" class="form-control form-control-user @error('nis') is-invalid @enderror" id="nis" placeholder="NIS Santri">
+                                        @error('nis')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                        @enderror
+                                    </div>
+                                    <br>
+
+                                    @if($nasabah)
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-12 col-form-label">Nama : {{$nasabah->nama}}</label>
+
+                                    </div>
+
+                                    @endif
+
+                                    <div class="form-group row">
+                                        <div class="col-sm-12 mb-12 mb-sm-0">
+                                            <input wire:model="password" type="password" pattern="[0-9]*" inputmode="numeric" id="pin" class="form-control form-control-user @error('password') is-invalid @enderror" placeholder="PIN">
+                                            @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                            @enderror
+                                        </div>
+
+                                    </div>
+                                    <br>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Verifikasi Bayar
+                                    </button>
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    <a class="small" href="/">Back to admin?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="/riwayat">Riwayat transaksi!</a>
+                                </div>
+                            </div>
                         </div>
-                        <br>
-                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                            Verifikasi Bayar
-                        </button>
-                    </form>
-                    <hr>
-                    <div class="text-center">
-                        <a class="small" href="/">Back to admin?</a>
-                    </div>
-                    <div class="text-center">
-                        <a class="small" href="/riwayat">Riwayat transaksi!</a>
+                        <!-- link gambar -->
+                        <div class="col-lg-5 d-none d-lg-block">
+                            <img src="assets/logo-transaksi.jpg">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</body>
 
 @push('scripts')
 <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
