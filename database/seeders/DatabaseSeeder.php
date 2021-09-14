b0975f12a1511aa580653b7d1923892d5edecd07
kurniagungk <kurniagungk@GMAIL.COM>
2021-09-14 17:57:46 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Saldo;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(userSeeder::class);
         $this->call(MitraSeeder::class);
         $this->call(SettingTabungan::class);
+        $this->call(Saldo::class);
     }
 }
