5926e7005cdba67f17e421641eaaf847917984a9
kurniagungk <kurniagungk@gmail.com>
2025-07-12 09:58:30 +0700

    Improve error logging for WhatsApp message failures with detailed information

--- app/Console/Commands/KirimPesanWhatsappCommand.php
+++ app/Console/Commands/KirimPesanWhatsappCommand.php
@@ -82,7 +82,7 @@ class KirimPesanWhatsappCommand extends Command
                     $this->info("Berhasil kirim untuk ID: {$pesan->id}");
                 } else {
                     $pesan->update(['status' => 'gagal']);
-                    $this->error("Gagal kirim untuk ID: {$pesan->id}" . ' - ' . $response->json());
+                    $this->error("Gagal kirim untuk ID: {$pesan->id}, {{$jid}}" . ' - ' . $response->json());
                 }
             } catch (\Exception $e) {
                 $pesan->update(['status' => 'gagal']);
