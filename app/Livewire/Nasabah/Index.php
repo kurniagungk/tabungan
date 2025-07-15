51619fca1c2e9f4bc97d21055de6dfbcf5ab1a18
kurniagungk <kurniagungk@gmail.com>
2025-07-15 17:41:00 +0700

    Add status filter and lembaga selection for admin in Nasabah index; enhance status toggle functionality

--- app/Livewire/Nasabah/Index.php
+++ app/Livewire/Nasabah/Index.php
@@ -2,18 +2,20 @@
 
 namespace App\Livewire\Nasabah;
 
-use App\Models\Nasabah;
+use App\Models\Saldo;
 
+use Mary\Traits\Toast;
+use App\Models\Nasabah;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Exports\NasabahExport;
-use App\Models\Saldo;
 use Maatwebsite\Excel\Facades\Excel;
 
 
 class Index extends Component
 {
     use WithPagination;
+    use Toast;
 
     public $confirming;
     public $sortField = 'nama';
@@ -21,8 +23,18 @@ class Index extends Component
     public $perPage = 10;
     public $sortAsc = true;
     public $firstPage = 1;
+    public $status = 'aktif'; // Default status filter
+    public $saldo_id = 'semua'; // Default saldo filter
 
     public array $sortBy = ['column' => 'nama', 'direction' => 'asc'];
+
+    public function mount()
+    {
+        $user = auth()->user();
+        $admin = $user->hasRole('admin');
+        if (!$admin)
+            $this->saldo_id =  $user->saldo_id;
+    }
 
 
     public function updatingPage($page)
@@ -35,10 +47,21 @@ class Index extends Component
         $this->confirming = $id;
     }
 
-    public function kill($id)
+    public function statusNasabah($id)
     {
-        Nasabah::destroy($id);
-        session()->flash('pesan', 'Data mitra successfully deleted.');
+        $nasabah = Nasabah::find($id);
+
+        $nasabah = Nasabah::find($id);
+
+        if (!$nasabah) {
+            return back()->with('pesan', 'Data nasabah tidak ditemukan.');
+        }
+
+        $nasabah->status = $nasabah->status === 'aktif' ? 'nonaktif' : 'aktif';
+
+        $nasabah->save();
+
+        $this->success('Berhasil', 'Status nasabah telah diperbarui.', 'toast-top toast-end', 'o-check-circle', 'alert-success');
     }
 
     public function export()
@@ -49,13 +72,16 @@ class Index extends Component
 
     public function render()
     {
-        $user = auth()->user();
-        $admin = $user->hasRole('admin');
+        $saldo_id = $this->saldo_id;
+        $status = $this->status;
 
 
-        $nasabah = Nasabah::when(!$admin, function ($query) use ($user) {
-            $query->where('saldo_id', $user->saldo_id);
+        $nasabah = Nasabah::when($saldo_id !== "semua", function ($query) use ($saldo_id) {
+            $query->where('saldo_id', $saldo_id);
         })
+            ->when($this->status !== 'semua', function ($query) use ($status) {
+                $query->where('status', $status);
+            })
             ->where(function ($query) {
                 $query->where('nama', 'like', '%' . $this->search . '%')
                     ->orWhere('rekening', 'like', '%' . $this->search . '%')
@@ -71,10 +97,17 @@ class Index extends Component
             ['key' => 'nama', 'label' => 'Nama', 'sortable' => true],
             ['key' => 'alamat', 'label' => 'Alamat'],
             ['key' => 'saldo', 'label' => 'Saldo'],
+            ['key' => 'status', 'label' => 'Status', 'sortable' => true],
+
         ];
 
+        $saldos = Saldo::select('id', 'nama')->get()->prepend((object)[
+            'id' => 'semua',
+            'nama' => 'Semua Lembaga'
+        ]);
 
 
-        return view('livewire.nasabah.index', compact("nasabah", "headers"));
+
+        return view('livewire.nasabah.index', compact("nasabah", "headers", "saldos"));
     }
 }
--- resources/views/livewire/nasabah/index.blade.php
+++ resources/views/livewire/nasabah/index.blade.php
@@ -7,7 +7,20 @@
     </div>
 
     <div class="mt-5 grid grid-cols-1 md:grid-cols-4 gap-5">
-        <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
+        <x-input placeholder="Search..." label="Search" wire:model.live.debounce="search" clearable
+            icon="o-magnifying-glass" />
+        <fieldset class="fieldset">
+            <legend class="fieldset-legend">Status Nasabah</legend>
+            <select class="select" wire:model.live.debounce="status">
+                <option value="semua">Semua</option>
+                <option value="aktif">Aktif</option>
+                <option value="tidak">Tidak Aktif</option>
+            </select>
+        </fieldset>
+        @role('admin')
+            <x-select label="Lembaga" wire:model.live.debounce="saldo_id" :options="$saldos" placeholder="Pilih Lembaga"
+                option-value="id" option-label="nama" />
+        @endrole
     </div>
 
     <x-table :headers="$headers" :rows="$nasabah" striped class="mt-5" with-pagination per-page="perPage"
@@ -21,9 +34,28 @@
                 class=" h-10 rounded-lg mx-auto" />
         @endscope
 
+        @scope('cell_saldo', $nasabah)
+            {{ Number::currency($nasabah->saldo, 'Rp.') }}
+        @endscope
+        @scope('cell_status', $nasabah)
+            <span class="badge {{ $nasabah->status === 'aktif' ? 'badge-success' : 'badge-error' }}">
+                {{ ucfirst($nasabah->status) }}
+            </span>
+        @endscope
         @scope('actions', $nasabah)
             <div class="flex gap-2">
+                <x-button icon="o-eye" link="/nasabah/{{ $nasabah->id }}/show" class="btn-sm btn-primary" />
                 <x-button icon="o-pencil" link="/nasabah/{{ $nasabah->id }}/edit" class="btn-sm btn-warning" />
+                @if ($nasabah->status === 'aktif')
+                    <x-button icon="o-power" wire:click="statusNasabah('{{ $nasabah->id }}')"
+                        wire:confirm.prompt="Akan menonaktifkan nasabah?\n\nTulis IYA to confirm|IYA"
+                        class="btn-sm  btn-error" />
+                @else
+                    <x-button icon="o-power" wire:click="statusNasabah('{{ $nasabah->id }}')"
+                        wire:confirm.prompt="Akan mengaktifkan nasabah?\n\nTulis IYA to confirm|IYA"
+                        class="btn-sm btn-success" />
+                @endif
+
             </div>
         @endscope
     </x-table>
