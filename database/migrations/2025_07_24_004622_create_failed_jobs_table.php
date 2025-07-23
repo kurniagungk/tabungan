e0072bbd093605083803115187d1206ff77a8772
kurniagungk <kurniagungk@gmail.com>
2025-07-24 00:47:16 +0700

    fix laravel job failed

--- database/migrations/2025_07_24_004622_create_failed_jobs_table.php
+++ database/migrations/2025_07_24_004622_create_failed_jobs_table.php
@@ -4,17 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
+        Schema::dropIfExists('failed_jobs');
+
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
+            $table->string('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
@@ -25,11 +26,9 @@ class CreateFailedJobsTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('failed_jobs');
     }
-}
+};
