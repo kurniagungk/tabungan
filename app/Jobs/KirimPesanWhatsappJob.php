4fad57de6334ee8aa2648166482ca7783444aefc
kurniagungk <kurniagungk@gmail.com>
2025-08-03 15:17:55 +0700

    fix retry

--- app/Jobs/KirimPesanWhatsappJob.php
+++ app/Jobs/KirimPesanWhatsappJob.php
@@ -15,6 +15,8 @@ class KirimPesanWhatsappJob implements ShouldQueue
 {
     use InteractsWithQueue, Queueable, SerializesModels;
 
+    public $tries = 3;
+
     /**
      * Create a new job instance.
      */
