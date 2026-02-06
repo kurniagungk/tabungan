1f38a11f85861dc6e333f1a6a01c6ec6ab0f8571
kurniagungk <kurniagungk@gmail.com>
2026-02-06 18:30:08 +0700

    refactor(vite): remove unused initTheme.js from input array

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/initTheme.js', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss()
